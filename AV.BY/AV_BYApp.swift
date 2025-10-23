a7884e2e4b3be6032419d1d0359aa7d2267f40a0
Kotylo <kirillkotylo@gmail.com>
2025-10-23 09:53:39 +0300

    Initial Commit

---
+++ AV.BY/AV_BYApp.swift
@@ -0,0 +1,17 @@
+//
+//  AV_BYApp.swift
+//  AV.BY
+//
+//  Created by Кирилл Котыло on 23.10.25.
+//
+
+import SwiftUI
+
+@main
+struct AV_BYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
